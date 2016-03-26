4f8623f047233cff1b35b11ca2734772a96c6662
aleksandrabliz <bliznina@genotek.ru>
2016-03-26 15:34:41 +0400

    Update commands.py
    Add changes in _cmd_reference(args)

--- cnvlib/commands.py
+++ cnvlib/commands.py
@@ -495,14 +495,25 @@ def _cmd_reference(args):
         antitargets = [f for f in filenames if 'antitarget' in f]
         logging.info("Number of target and antitarget files: %d, %d",
                      len(targets), len(antitargets))
-        if not args.tthreshold and not args.athreshold:
-            ref_probes = do_reference(targets, antitargets, args.fasta,
-                                      args.male_reference,
-                                      args.do_gc, args.do_edge, args.do_rmask)
-        elif args.tthreshold and args.athreshold:
+        if args.tthreshold and args.athreshold:
+            print "Thresholds for gender determination: ", args.tthreshold, "for target and", args.athreshold, "for antitarget."
             ref_probes = do_reference(targets, antitargets, args.fasta,
                                       args.male_reference,
                                       args.do_gc, args.do_edge, args.do_rmask, args.tthreshold, args.athreshold)
+        if args.tthreashold and not args.athreshold:
+            print "Threshold for gender determination by target:", args.tthreshold
+            ref_probes = do_reference(targets, antitargets, args.fasta,
+                                      args.male_reference,
+                                      args.do_gc, args.do_edge, args.do_rmask, target_threshold = args.tthreshold)
+        if args.athreshold and not args.tthreshold:
+            print "Threshold for gender determination by antitarget:", args.athreshold
+            ref_probes = do_reference(targets, antitargets, args.fasta,
+                                      args.male_reference,
+                                      args.do_gc, args.do_edge, args.do_rmask, antitarget_threshold = args.athreshold)
+        else:
+            ref_probes = do_reference(targets, antitargets, args.fasta,
+                                      args.male_reference,
+                                      args.do_gc, args.do_edge, args.do_rmask)
     else:
         raise ValueError(usage_err_msg)
 
